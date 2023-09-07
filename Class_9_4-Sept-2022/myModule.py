eba9a18d006423558828ab428037a7918224316c
Kawish Akber <96989937+iamkawish@users.noreply.github.com>
2023-09-07 21:11:58 +0500

    Add files via upload

---
+++ Class_9_4-Sept-2022/myModule.py
@@ -0,0 +1,26 @@
+# a Module can have function, classes, variable
+def add(num1,num2):
+    """Docstring:
+add(value1,value2)
+
+Sums the values.
+return and numerical value incase of integers are supplied 
+return string value in case of string are supplied"""
+    return num1+ num2
+
+
+def sub(num3,num4):
+    """Docstring:
+add(value1,value2)
+
+Subtract the values.
+return and numerical value incase of integers are supplied 
+return string value in case of string are supplied"""
+    return num3-num4
+
+
+def circumference(radius):
+    return (2*3.142*radius)
+
+def conversion(km):
+    return km*1000 
---
+++ Class_9_4-Sept-2022/myModule1.py
@@ -0,0 +1,2 @@
+def conversion(km):
+    return ("The Km is Convert to Meter",km*1000)
